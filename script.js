5404c5f6b42c63ffbb9ff7b80dbef18f06872d80
Caio Emmanuel <88410208+CaioEOS@users.noreply.github.com>
2022-11-16 10:17:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -63,11 +63,7 @@ function adicionar() {
   var profissao = document.getElementById("profissao").value;
   var email = document.getElementById("email").value;
   var telefone = document.getElementById("telefone").value;
-  const hoje = new Date()
-  const dia = hoje.getDate().toString().padStart(2,'0')
-  const mes = String(hoje.getMonth() + 1).padStart(2,'0')
-  const ano = hoje.getFullYear()
-  const dataAtual = `${dia}/${mes}/${ano}`
+  const hoje = new new Date().toLocaleDateString()
   pessoas.push({name:nome, age:idade, profissao:profissao, email:email, telefone:telefone, datapreec:dataAtual});
   console.log(pessoas);
 }
